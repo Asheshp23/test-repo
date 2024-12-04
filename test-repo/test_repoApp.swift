efe9f0c40afc582bf636c52a34d29e816fd1c829
asheshp23 <asheshp23@gmail.com>
2024-12-04 10:31:30 -0500

    initial commit

---
+++ test-repo/test_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_repoApp.swift
+//  test-repo
+//
+//  Created by Ashesh Patel on 2024-12-04.
+//
+
+import SwiftUI
+
+@main
+struct test_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
